0953eb11eece19bbf60be6fe952a7191af2f4dad
SenehaGhosh <senehaghosh2378@gmail.com>
2026-04-24 17:43:48 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const images = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+let currentIndex = 0;
+
+images.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    currentIndex = index;
+    showImage();
+    lightbox.style.display = "flex";
+  });
+});
+
+function showImage() {
+  lightboxImg.src = images[currentIndex].src;
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage();
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage();
+}
+
+closeBtn.onclick = () => {
+  lightbox.style.display = "none";
+};
